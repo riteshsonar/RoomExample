176be6db9084978271507048bc08bf9b8b7aa740
riteshsonar <ritesh.sonar111@gmail.com>
2022-01-16 23:54:25 +0530

    Add Convertors

--- app/src/main/java/com/ritesh/roomexample/ContactDatabase.kt
+++ app/src/main/java/com/ritesh/roomexample/ContactDatabase.kt
@@ -1,16 +1,16 @@
 package com.ritesh.roomexample
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
 
 @Database(entities = [Contact::class], version = 1)
+@TypeConverters(Convertors::class)
 abstract class ContactDatabase: RoomDatabase() {
 
     abstract fun contactDao(): ContactDao
 
     companion object{
+        @Volatile
         private var INSTANCE: ContactDatabase? =null
 
         fun getDatabase(context: Context): ContactDatabase{
---
+++ app/src/main/java/com/ritesh/roomexample/Convertors.kt
@@ -0,0 +1,15 @@
+package com.ritesh.roomexample
+
+import androidx.room.TypeConverter
+import java.util.*
+
+class Convertors {
+    @TypeConverter
+    fun fromDateToLong( value: Date): Long{
+        return value.time
+    }
+    @TypeConverter
+    fun fromLongToDate(value: Long): Date{
+        return Date(value)
+    }
+}
