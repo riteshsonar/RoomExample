61e7e507d2fc3d43be61a96e8791d4f4a6c18e72
riteshsonar <ritesh.sonar111@gmail.com>
2022-01-19 23:30:01 +0530

    Add Convertors

---
+++ app/src/main/java/com/ritesh/roomexample/SceondActivity.kt
@@ -0,0 +1,11 @@
+package com.ritesh.roomexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SceondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sceond)
+    }
+}
